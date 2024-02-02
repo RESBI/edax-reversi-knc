d539b42f5841b349016f2a4688a828c692ca78c2
Resbi <resbi@redstone.link>
2024-02-02 17:26:11 +0800

    Larger MAX_THREADS.
    Set it higher to fit maximum 61*4=244 threads of Xeon Phi x100.

--- src/const.h
+++ src/const.h
@@ -12,7 +12,7 @@
 #define EDAX_CONST_H
 
 /** maximal number of threads */
-#define MAX_THREADS 64
+#define MAX_THREADS 512 // Set it higher to fit maximum 61*4=244 threads of Xeon Phi x100.
 
 /** maximal number of moves */
 #define MAX_MOVE 32
